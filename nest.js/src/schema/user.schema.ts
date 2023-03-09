2152d98f10f0464fcdd984450ec722f6b1a9e7ba
mdshehabsk <mdshehabsk@gmail.com>
2023-03-09 16:20:21 +0600

    trying to bug fix

--- nest.js/src/schema/user.schema.ts
+++ nest.js/src/schema/user.schema.ts
@@ -1,6 +1,6 @@
-import mongoose from 'mongoose';
+import {Schema} from 'mongoose';
 
-export const UserSchema = new mongoose.Schema({
+export const UserSchema = new Schema({
   name: String,
   email: String,
   password: String,
